56ac0b630fb2e0c283c10af5ea221c0f96148e88
Daniel Huppmann <dh@dergelbesalon.at>
2021-02-15 23:21:11 +0100

    Add the iiasa design colours via a css file

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -56,3 +56,7 @@ html_logo = '_static/iiasa_logo_white.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Add the IIASA stylesheet
+def setup(app):
+    app.add_css_file('iiasa.css')
